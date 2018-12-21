7762bc4acbb2f526d135ad60bca7ab4c6b477d10
Skender <alexasp3@gmail.com>
2018-12-21 12:12:30 +0300

    Реализация списка услуг

---
+++ app/src/main/java/com/photosaloon/ui/services/Services.java
@@ -0,0 +1,11 @@
+package com.photosaloon.ui.services;
+
+import com.photosaloon.R;
+import com.photosaloon.ui.base.BaseActivity;
+
+public class Services extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_services;
+    }
+}
