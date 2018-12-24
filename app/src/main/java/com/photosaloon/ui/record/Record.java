f245588f8a0c487f4abecd8694f1d49594075531
Skender <alexasp3@gmail.com>
2018-12-24 14:34:32 +0300

    Вестка экрана записи

--- app/src/main/java/com/photosaloon/ui/record/Record.java
+++ app/src/main/java/com/photosaloon/ui/record/Record.java
@@ -1,11 +1,23 @@
 package com.photosaloon.ui.record;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import com.photosaloon.R;
 import com.photosaloon.ui.base.BaseFragment;
 
 public class Record extends BaseFragment {
+
+
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_new_record;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 }
